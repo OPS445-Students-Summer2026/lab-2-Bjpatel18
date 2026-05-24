e7fa4d2f55684cb3e5365d81ccd33163175913b4
Bhavya J Patel <bjpatel18@myseneca.ca>
2026-05-24 14:58:45 -0400

    Completed Lab 2

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Bhavya J Patel
+# Author ID: bjpatel18
+# Date Created: 2026/05/24
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
